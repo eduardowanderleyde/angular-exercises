04c0425f36e025534fde8d29e6fd8764f05bf28a
Eduardo Wanderley de Siqueira Araújo <wanderley.eduardo@gmail.com>
2025-08-19 22:39:01 -0300

    feat: initialize Angular 17+ project with standalone components

---
+++ advanced-angular-practice/src/app/components/task-stats/task-stats.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-stats',
+  imports: [],
+  templateUrl: './task-stats.html',
+  styleUrl: './task-stats.scss'
+})
+export class TaskStats {
+
+}
